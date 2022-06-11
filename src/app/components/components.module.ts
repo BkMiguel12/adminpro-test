e72644f9147646edad7ecc577ab7356c6fe32bad
Miguel Mendoza <miguel@lisainsurtech.com>
2022-06-11 00:01:30 -0400

    Updating angular version

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 import { IncrementComponent } from './increment/increment.component';
 import { DonaGraphicsComponent } from './dona-graphics/dona-graphics.component';
@@ -18,7 +18,7 @@ import { DonaGraphicsComponent } from './dona-graphics/dona-graphics.component';
   imports: [
     CommonModule,
     FormsModule,
-    ChartsModule
+    NgChartsModule
   ]
 })
 
